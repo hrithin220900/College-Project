e812943cfedf610e88952ccd588b12567323ae75
hrithin220900 <65213514+hrithin220900@users.noreply.github.com>
2023-04-04 17:31:53 +0530

    app get request for server

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const SERVER = "https://virtualacharya-server.pages.dev/api" 
+export const SERVER = "https://virtualacharya-server.vercel.app/api" 
 
 export const subjects = [
     "math", "physics", "dbms", "Machine Learning", "FLAT", "Compiler Design"
